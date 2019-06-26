68b36c5dbda84f82dfdf46f656a4b8c2551c0dfe
Baezl <macair@B-Macbook-Pro.local>
2019-06-26 23:25:54 +0200

    Book ReviewPad Added Postbuild

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const { auth} = require('./middleware/auth')
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'))
+
 // GET //
 app.get('/api/auth',auth,(req,res)=>{
     res.json({
@@ -154,6 +156,14 @@ app.delete('/api/delete_book',(req,res)=>{
         res.json(true)
     })
 })
+
+
+if (process.env.NODE_ENV === 'production'){
+        const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 3001;
